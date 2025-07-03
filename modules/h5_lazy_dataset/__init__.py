675a205edf3938a9c2044806b43e9be2c424b9a1
LingWL02 <154620085+LingWL02@users.noreply.github.com>
2025-07-04 00:14:06 +0800

    Added transformation pipelining for H5LazyDataset.

--- modules/h5_lazy_dataset/__init__.py
+++ modules/h5_lazy_dataset/__init__.py
@@ -1,7 +1,8 @@
-import h5py
 import ast
-from typing import Any
+from typing import Any, Callable, Optional, Tuple
+from functools import reduce
 
+import h5py
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -9,34 +10,40 @@ from torch.utils.data import Dataset, DataLoader
 
 class H5LazyDataset(Dataset):
 
-    def _get_file(self) -> h5py.File:
-        if self._file is None:
-            self._file = h5py.File(self.path, 'r')
-
-        return self._file
-
-    def __init__(self, path: str, data_key: str, label_key: str) -> None:
+    def __init__(
+        self, path: str, data_key: str, label_key: str,
+        data_transforms: Tuple[Callable[[Any], Any], ...] = (),
+        label_transforms: Tuple[Callable[[Any], Any], ...] = (),
+    ) -> None:
         self._file: h5py.File | None = None
         self.path: str = path
         self.data_key: str = data_key
         self.label_key: str = label_key
+        self.data_transforms: Tuple[Callable[[Any], Any], ...] = data_transforms
+        self.label_transforms: Tuple[Callable[[Any], Any], ...] = label_transforms
 
-        with h5py.File(self.path, 'r') as f:
-            self.length = len(f[self.label_key])
+        self.length = 0
 
     def __len__(self) -> int:
         return self.length
 
-    def __getitem__(self, index) -> Any:
-        f = self._get_file()
-        data: torch.Tensor = torch.tensor(f[self.data_key][index])
+    def __getitem__(self, index) -> Tuple[Any, Any]:
+        f = self.get_file()
+        data: Any = f[self.data_key][index]
 
-        raw_label: np.bytes_ = f[self.label_key][index]
-        str_label: str = raw_label.decode('utf-8')
-        label: torch.Tensor = torch.tensor(ast.literal_eval(str_label))
+        label: Any = f[self.label_key][index]
 
-        return data, label
+        data_transformed: torch.Tensor = reduce(lambda _data, transformer: transformer(_data), self.data_transforms, data)
+        label_transformed: torch.Tensor = reduce(lambda _label, transformer: transformer(_label), self.label_transforms, label)
+
+        return data_transformed, label_transformed
 
     def __del__(self) -> None:
         if self._file is not None:
             self._file.close()
+
+    def get_file(self) -> h5py.File:
+        if self._file is None:
+            self._file = h5py.File(self.path, 'r')
+
+        return self._file
