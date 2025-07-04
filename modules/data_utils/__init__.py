180a3a6812590d1af4729534904a2a7efff791f1
LingWL02 <154620085+LingWL02@users.noreply.github.com>
2025-07-04 21:14:42 +0800

    Added support for testdataset.

--- modules/data_utils/__init__.py
+++ modules/data_utils/__init__.py
@@ -9,30 +9,35 @@ from torch.utils.data import Dataset
 class H5LazyDataset(Dataset):
 
     def __init__(
-        self, path: str, data_key: str, label_key: str,
+        self, path: str, data_key: str | None = None, label_key: str | None = None,
         data_transforms: Tuple[Callable[[Any], Any], ...] = (),
         label_transforms: Tuple[Callable[[Any], Any], ...] = (),
     ) -> None:
         self._file: h5py.File | None = None
         self.path: str = path
-        self.data_key: str = data_key
-        self.label_key: str = label_key
+        self.data_key: str | None = data_key
+        self.label_key: str | None = label_key
         self.data_transforms: Tuple[Callable[[Any], Any], ...] = data_transforms
         self.label_transforms: Tuple[Callable[[Any], Any], ...] = label_transforms
 
-        self.length = 0
+        self.length = len(self.get_file())
 
     def __len__(self) -> int:
         return self.length
 
-    def __getitem__(self, index) -> Tuple[Any, Any]:
-        f: h5py.File = self.get_file()
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        file: h5py.File = self.get_file()
 
-        data: Any = cast(h5py.Dataset, f[self.data_key])[index]
-        label: Any = cast(h5py.Dataset, f[self.label_key])[index]
+        data_transformed: Any = None
+        label_transformed: Any = None
 
-        data_transformed: torch.Tensor = reduce(lambda _data, transformer: transformer(_data), self.data_transforms, data)
-        label_transformed: torch.Tensor = reduce(lambda _label, transformer: transformer(_label), self.label_transforms, label)
+        if self.data_key is not None:
+            data = cast(h5py.Dataset, file[self.data_key])[index]
+            data_transformed = reduce(lambda _data, transformer: transformer(_data), self.data_transforms, data)
+
+        if self.label_key is not None:
+            label: Any = cast(h5py.Dataset, file[self.label_key])[index]
+            label_transformed = reduce(lambda _label, transformer: transformer(_label), self.label_transforms, label)
 
         return data_transformed, label_transformed
 
